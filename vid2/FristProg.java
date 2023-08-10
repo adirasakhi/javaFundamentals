d44e8e656de115724840f8b54d44e76120b9e533
adirasakhi <100334339+adirasakhi@users.noreply.github.com>
2023-08-10 16:21:41 +0700

    Lanjutan

---
+++ vid2/FristProg.java
@@ -0,0 +1,8 @@
+public class FristProg{
+    public static void main( String[] args){
+        System.out.println("i am determined to learn how to code.");
+        System.out.println("Today's date is");
+
+    }
+
+}
